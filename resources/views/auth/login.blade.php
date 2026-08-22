600fd4db5d682e56e4db917841b50f8bd08cbe02
komangyxy-commits <ikomangriskytriyanaputra@gmail.com>
2026-08-23 00:07:59 +0800

    menambahkan style login user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,28 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 <body>
-    <h1>Halaman Login</h1>
-    @if(session('success'))
-        <div style="color: green; padding: 10px; border: 1px solid green; margin-bottom: 15px;">
-            {{ session('success') }}
-        </div>
-    @endif
+    <main class="form-page">
+        <section class="form-card">
+            <div class="form-header">
+                <h1>Login</h1>
+                <p>Masuk ke akun MyTodo kamu.</p>
+            </div>
 
-    <form action="{{ route('login') }}" method="POST">
-    @csrf
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" required><br>
+            @if(session('success'))
+                <p class="success-message">
+                    {{ session('success') }}
+                </p>
+            @endif
 
-        @error('email')
-            <div>{{ $message }}</div>
-        @enderror
-        
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required><br>
+            <form action="{{ route('login') }}" method="POST">
+            @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="contoh@email.com" required><br>
 
-        <button type="submit">Login</button>
-    </form>
+                    @error('email')
+                        <div>{{ $message }}</div>
+                    @enderror
+                </div>
+                    
+                 <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="********" required><br>
+
+                    @error('password')
+                        <p class="form-error">
+                            {{ $message }}
+                        </p>
+                    @enderror
+
+                    <button type="submit" class="btn-submit">Login</button>
+                </div>
+            </form>
+            <p class="auth-link">Belum punya akun?<a href="{{ route('register') }}">Daftar</a></p>
+        </section>
+    </main>
 </body>
 </html>
